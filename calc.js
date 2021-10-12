1b7a914398c104547637b9f81ee2efb06149dfdb
Ponesty <92332740+Ponesty@users.noreply.github.com>
2021-10-12 15:10:42 -0500

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,42 @@
+const readline = require('readline');
+
+const reader = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+
+
+reader.question("What would you like to calculate?", function(input){
+	tokens = input.split(' ');
+	
+	mathSymbol = tokens[0];
+	num1 = Number(tokens[1]);
+	num2 = Number(tokens[2]);
+
+	let answer, answer2;
+	if(mathSymbol == '+'){
+		answer = num1+num2;
+	}
+	else if(mathSymbol == '-'){
+		answer = num1-num2;
+	}
+	else if(mathSymbol == '*'){
+		answer = num1*num2;
+	}
+	else if(mathSymbol == '/'){
+		answer = num1 / num2;
+	}
+	else if(mathSymbol == '√'){
+		answer = Math.sqrt(num1 + num2);
+		//answer2 = Math.sqrt(num2);
+		//console.log(answer , answer2);
+	}
+	console.log(answer);
+
+
+	// This line closes the connection to the command line interface.
+	reader.close();
+	
+
+});
---
+++ maxOfTwo.js
@@ -0,0 +1,12 @@
+let x = 1;
+let y = 2;
+if(x>y){
+    console.log(x);
+
+}
+else if(x=== y){
+    console.log("The values are identical.");
+}
+else{
+    console.log(y);
+}
