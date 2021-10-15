c501d06ce98a4975239bf7ae66a6d8a5a8303d64
Jerrell Prier <92332740+Ponesty@users.noreply.github.com>
2021-10-15 12:23:29 -0500

    Add files via upload
    Adding lab to correct repository

---
+++ findTheRange.js
@@ -0,0 +1,13 @@
+let arr = [1,4,11,2,37,-4];
+let min= arr[0];
+let max = arr[0];
+
+for(let i =0; i< arr.length; i++){
+    if(arr[i] < min){
+        min = arr[i];
+    }
+    if (arr[i] > max){
+        max = arr[i];
+    }
+}
+console.log(min, max);
