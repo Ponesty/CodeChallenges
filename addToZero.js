e56894af4370845ca3546635063a7f2561f4206b
Jerrell Prier <jerrellprier@gmail.com>
2021-10-18 10:10:04 -0500

    Finished Challenge 10/18/21

--- addToZero.js
+++ addToZero.js
@@ -1,4 +1,21 @@
 // Starting array
 let array = [28, 43, -12, 30, 4, 0, 12];
 
+
 // Write your solution below:
+function out(array){
+    let answer = false;
+    let count = 1;
+    for(let i = 0; i< array.length; i++){
+        for(let j =count; j< array.length; j++){
+            if(array[i]+array[j]===0){
+                answer = true;
+            }
+        }
+        count++;
+        
+    }
+    return answer;
+    
+}
+console.log(out(array));
