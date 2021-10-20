4f043c204b9af802f2c5311fc97ff49458cb88d0
Jerrell Prier <jerrellprier@gmail.com>
2021-10-20 10:03:17 -0500

    Had issues, kept printing out true for all string inputs to functions. I realized I needed to add a count to increment j in the second for loopoutside of second loop but inside for loop.

--- hasUniqueChars.js
+++ hasUniqueChars.js
@@ -1,1 +1,20 @@
 // Write your code below
+function hasUniqueChars(word){
+    let boo = true;
+    let count = 1;
+    for(let i=0; i<word.length; i++){
+        for(let j= count; j<word.length; j++){
+            if(word[i] === word[j]){
+                boo = false;
+                
+            }
+
+        }
+        count++;
+        
+    }
+    return boo;
+}
+
+console.log(hasUniqueChars('Monday'));
+console.log(hasUniqueChars('Mondday'));
