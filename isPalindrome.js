eec4878c092580d203ba23f5a09da58fe57b93b9
Jerrell Prier <jerrellprier@gmail.com>
2021-10-21 09:35:34 -0500

    Coding Challenge 10/21/21. Added a solution that uses a loop instead of using only methods. Either one works. If you want to use one function, make sure the other one is commented out.

--- isPalindrome.js
+++ isPalindrome.js
@@ -1,13 +1,33 @@
 // Write your code below
 
+//Without loop
+// const isPalindrome = w => {
+//     let x;
+//     x = w.split('').reverse().join('');
+//     if(w === x){
+//         return true;
+//     }
+//     else{
+//         return false;
+//     }
+// }
+
+
+//With loop
 const isPalindrome = w => {
-    let x;
-    x = w.split('').reverse().join('');
-    if(w === x){
+    let y = [];
+    for(let i = w.length -1; i>= 0; i--){
+        y.push(w[i]);
+    }
+    y = y.join('');
+    if(w === y){
         return true;
     }
     else{
         return false;
     }
+    
 }
+
+
 console.log(isPalindrome('racecar'));
