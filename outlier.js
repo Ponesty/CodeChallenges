c9da41c3fa493e7782825ee53e9fc97bdcc2593f
Jerrell Prier <jerrellprier@gmail.com>
2021-11-08 09:12:14 -0600

    11/8/21 coding challenge. Used filter method to filter even and odd numbers in array and returned the array with the least amount of numbers.

---
+++ outlier.js
@@ -0,0 +1,13 @@
+const outlier = arr =>{
+    let oCount = [];
+    let eCount = [];
+    oCount = arr.filter((number) => number%2!==0)
+    eCount = arr.filter((number) => number%2===0)
+    if(eCount.length > oCount.length){
+        return oCount[0];
+    } else{
+        return eCount[0];
+    }
+}
+
+console.log(outlier([2, 4, 0, 100, 4, 11, 2602, 36]));
